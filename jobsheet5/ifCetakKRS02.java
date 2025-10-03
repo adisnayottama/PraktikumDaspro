d9628e16dbe823ccd4b267a878f4daea7ec998a0
adisnayottama <adisnayottama1@gmail.com>
2025-10-03 13:36:29 -0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS02.java
+++ jobsheet5/ifCetakKRS02.java
@@ -11,11 +11,11 @@ public class ifCetakKRS02 {
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-   System.out.println(
-    uktLunas 
-    ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
-    : ""
-);
-
+   if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
 }
